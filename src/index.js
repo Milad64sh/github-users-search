948bd852fbb171b5ebb9ec8297569341d81e9109
Milad64sh <m.shalikarian@gmail.com>
2022-07-16 16:16:34 -0700

    login and logout

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-44kz2ciq.us.auth0.com'
+      clientId='Sh2MtIKdcVAAk0N0tjTUg5cTdr4WkVfM'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
